e7b4464a3bd94ddb2dbcf153672b8ba0e7b2ed89
sergent <fayesergent@gmail.com>
2019-05-21 09:46:38 +0000

    thiakhane

--- src/main/java/parking/business/Voiture.java
+++ src/main/java/parking/business/Voiture.java
@@ -7,7 +7,7 @@ public class Voiture extends Vehicule
 {
 	private static final long serialVersionUID = 8047176247375201388L;
 
-	public Voiture(String imme, String mode, String mare, String propeeee)
+	public Voiture(String imme, String mode, String mareeeeeeeee, String propeeee)
 	{
 		super(imm, mod, mar, prop);
 	}
